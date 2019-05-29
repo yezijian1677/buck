464fca3ea51b4fe32fa076c362de7d02f4102046
yezijian <1677390657@qq.com>
2019-05-29 21:37:26 +0800

    init

---
+++ src/main/java/cur/pro/buck/BuckApplication.java
@@ -0,0 +1,13 @@
+package cur.pro.buck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cur/pro/buck/BuckApplicationTests.java
@@ -0,0 +1,16 @@
+package cur.pro.buck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuckApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
